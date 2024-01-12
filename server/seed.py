8efd0614f47e117aad0bcd7348393a7328766694
faithwest <faithchepkoech425@gmail.com>
2024-01-12 12:18:29 +0300

    populated seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,35 +1,31 @@
-#!/usr/bin/env python3
-
 from random import choice as rc
+
 from faker import Faker
+
 from app import app
 from models import db, Owner, Pet
 
-# Populate db
-def seed_data():
-    owner1 = Owner(name="marcus",pets="flax")
-    owner2 = Owner(name="gustavo",pets="whiskers")
+db.init_app(app)
 
-    db.session.add(owner1)
-    db.session.add(owner2)
-    db.session.commit() 
+fake = Faker()
 
+with app.app_context():
 
-    pet1=Pet(name="flax", species="lebron", owner_id=owner2.id)
-    pet2=Pet(name="whiskers", species="sango", owner_id=owner1.id)
+    Pet.query.delete()
+    Owner.query.delete()
 
-    #commit
-    db.session.add(pet1)
-    db.session.add(pet2)
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
     db.session.commit()
-
-
-
-if __name__ =='__main__':
-    with app.app_context():
-        print("Seeding started -----")
-        #seed_data()
-        print("Seeded successfully")
-
-
-    
